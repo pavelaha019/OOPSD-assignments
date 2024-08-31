4443758d3369830f7e1471afe9572a4cba4204fe
Pavel Ahamed <121998324+pavelaha019@users.noreply.github.com>
2024-08-31 23:55:36 +0600

    Add files via upload

---
+++ Assignment03_221074002_task 01/src/assignment03_221074002_task/pkg01/Assignment03_221074002_task01.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
+ */
+package assignment03_221074002_task.pkg01;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author pavel ahamed
+ */
+public class Assignment03_221074002_task01 extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("LoginFXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
